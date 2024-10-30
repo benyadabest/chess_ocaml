6d01fb7e67297efee1bd7a21dfc5d66d5c3bd536
DylanD75 <dmd294@cornell.edu>
2024-10-30 00:39:40 -0400

    Implement base code for game.ml and game.mli

--- lib/game.ml
+++ lib/game.ml
@@ -1,9 +1,10 @@
 open Pieces
 open Board
+open Pieces
 
 type game_state = {
   board : board;
-  current_player : color;
+  is_whites_move : bool;
   in_check : bool;
   game_over : bool;
 }
@@ -11,7 +12,28 @@ type game_state = {
 let init_game () : game_state =
   {
     board = init_board ();
-    current_player = White;
+    is_whites_move = true;
     in_check = false;
     game_over = false;
   }
+
+let make_move game_state start_pos end_pos end_state =
+  let start_piece = Board.get_piece_at game_state start_pos in
+  match start_piece with
+  | Some piece -> (
+      let color = piece.color in
+      let piece_type = piece.piece_type in
+      match piece_type with
+      | King -> failwith "continue implementation"
+      | Queen -> failwith "continue implementation"
+      | Rook -> failwith "continue implementation"
+      | Bishop -> failwith "continue implementation"
+      | Knight -> failwith "continue implementation"
+      | Pawn -> failwith "continue implementation")
+  | None ->
+      failwith
+        "Error: Cannot make move because there is no piece at the selected \
+         start position."
+
+let game_over game_state = game_state.game_over
+let is_whites_move game_state = game_state.is_whites_move
--- lib/game.mli
+++ lib/game.mli
@@ -1,19 +1,20 @@
-(** Represents the state of the game. *)
 type game_state
+(** Represents the state of the game. *)
 
-(** Initializes a new game. Returns the initial game state. *)
 val init_game : unit -> game_state
+(** Initializes a new game. Returns the initial game state. *)
 
-(** Makes a move in the game.
-    [game_state] is the current state of the game.
-    [start_pos] is the (row, col) of the piece to move.
-    [end_pos] is the (row, col) where the piece is moving to.
-    Returns the updated game state and a result indicating the status of the game. *)
-val make_move : game_state -> (int * int) -> (int * int) -> game_state * string
+val make_move : game_state -> int * int -> int * int -> game_state * string
+(** Makes a move in the game. [game_state] is the current state of the game.
+    [start_pos] is the (row, col) of the piece to move. [end_pos] is the (row,
+    col) where the piece is moving to. Returns the updated game state and a
+    result indicating the status of the game. Raises a `Failure` if [start_pos]
+    or [end_pos] is not a valid coordinate on the board, or if the move from
+    [start_pos] to [emd_pos] is not a valid move. *)
 
-(** Checks if the game has ended.
-    Returns true if the game is over (checkmate, stalemate, or draw), false otherwise. *)
 val game_over : game_state -> bool
+(** Checks if the game has ended. Returns true if the game is over (checkmate,
+    stalemate, or draw), false otherwise. *)
 
-(** Retrieves the current player's turn (White or Black). *)
-val current_turn : game_state -> Pieces.color
+val is_whites_turn : game_state -> bool
+(** Checks whether or not it's white's turn to move. *)
