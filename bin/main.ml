9a10c55f434fa3d2a60fb292005fec62f9e56dcf
Dylan Dehnert <164942331+DylanD75@users.noreply.github.com>
2024-12-12 15:40:31 -0500

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,42 +1,71 @@
-open Chess
+open Chess.Game
+open Printf
 
-(* Print the initial board state *)
-let print_initial_board () =
-  let board = Board.init_board () in
-  Board.print_board board
+let read_coords prompt =
+  print_string prompt;
+  flush stdout;
+  let input = read_line () in
+  try
+    let r, c = Scanf.sscanf input "%d %d" (fun r c -> (r, c)) in
+    (r, c)
+  with _ ->
+    print_endline "Invalid input. Please enter two integers like '1 4'.";
+    read_coords prompt
 
-(* Move every piece at least once *)
-let move_every_piece () =
-  let board = ref (Board.init_board ()) in
+let rec perform_move game =
+  let color_str = if is_whites_move game then "White" else "Black" in
+  printf "%s's turn.\n" color_str;
+  print_board game;
 
-  (* Define a series of valid moves for each piece type *)
-  let moves = [
-    ((6, 0), (4, 0)); (* White Pawn *)
-    ((1, 0), (3, 0)); (* Black Pawn *)
-    ((7, 1), (5, 2)); (* White Knight *)
-    ((0, 1), (2, 2)); (* Black Knight *)
-    ((7, 2), (5, 0)); (* White Bishop *)
-    ((0, 2), (2, 0)); (* Black Bishop *)
-    ((7, 0), (5, 0)); (* White Rook *)
-    ((0, 0), (2, 0)); (* Black Rook *)
-    ((7, 3), (5, 1)); (* White Queen *)
-    ((0, 3), (2, 1)); (* Black Queen *)
-    ((7, 4), (6, 4)); (* White King *)
-    ((0, 4), (1, 4)); (* Black King *)
-  ] in
+  let start_coords =
+    read_coords
+      "Enter the coordinates of the piece you want to move (e.g. '1 4'): "
+  in
+  let piece_opt = Chess.Board.get_piece_at game.board start_coords in
+  match piece_opt with
+  | None ->
+      print_endline "No piece at that square!";
+      perform_move game
+  | Some piece -> (
+      let piece_color = Chess.Pieces.get_color piece in
+      if
+        (is_whites_move game && piece_color <> "White")
+        || ((not (is_whites_move game)) && piece_color <> "Black")
+      then begin
+        print_endline "That piece does not belong to you!";
+        perform_move game
+      end
+      else
+        let end_coords =
+          read_coords
+            "Enter the coordinates to move this piece to (e.g. '3 4'): "
+        in
+        try
+          let new_game = move game start_coords end_coords in
+          print_endline "Move successful!";
+          new_game
+        with Failure msg ->
+          print_endline msg;
+          perform_move game
 
-  (* Execute the moves and print the board after each move *)
-  List.iter (fun (start_pos, end_pos) ->
-    Printf.printf "Moving piece from %s to %s\n"
-      (Board.indices_to_algebraic start_pos)
-      (Board.indices_to_algebraic end_pos);
-    board := Board.move_piece !board start_pos end_pos;
-    Board.print_board !board;
-    print_endline ""
-  ) moves
+let rec game_loop game =
+  if game_over game then begin
+    if
+      Board.is_checkmate game.board
+        (if is_whites_move game then White else Black)
+    then
+      let winner = if is_whites_move game then "Black" else "White" in
+      printf "Checkmate! %s wins!\n" winner
+    else if
+      Board.is_stalemate game.board
+        (if is_whites_move game then White else Black)
+    then print_endline "Stalemate! It's a draw!"
+    else print_endline "Game over!"
+  end
+  else
+    let new_game = perform_move game in
+    game_loop new_game
 
 let () =
-  print_endline "Initial Board State:";
-  print_initial_board ();
-  print_endline "\nPerforming moves:";
-  move_every_piece ()
+  let game = init_game () in
+  game_loop game
